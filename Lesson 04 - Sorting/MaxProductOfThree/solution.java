a2c21b9f20ad1c45c21c16cb2844b3cdad1e04a6
Ghanan Gowripalan <ghanan94@gmail.com>
2015-07-29 21:07:16 -0400

    Added my solution (in Java) to Lesson 4's MaxProductOfThree

---
+++ Lesson 04 - Sorting/MaxProductOfThree/solution.java
@@ -0,0 +1,55 @@
+// Code written in Java
+// Correctness: 100 %
+// Performance: 100 %
+// Time Complexity: O(N * log(N))
+// Space Complexity: O(1)
+
+class Solution {
+    public int solution(int[] A) {
+        int N = A.length;
+        quickSort(A, 0, N - 1);
+
+        int tmp1 = (A[0] * A[1]);
+        int tmp2 = (A[N - 2] * A[N - 3]);
+
+        if (A[N - 1] > 0) {
+            return A[N - 1] * ((tmp1 > tmp2) ? tmp1 : tmp2);
+        } else {
+            return A[N - 1] * ((tmp1 < tmp2) ? tmp1 : tmp2);
+        }
+    }
+
+    private void quickSort(int arr[], int left, int right) {
+        int index = partition(arr, left, right);
+
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+
+    private int partition(int arr[], int left, int right) {
+        int pivot = arr[(left + right) / 2];
+
+        while (left <= right) {    
+            while (arr[left] < pivot) {
+                left++;
+            }
+
+            while (arr[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                int tmp = arr[left];
+                arr[left++] = arr[right];
+                arr[right--] = tmp;
+            }
+        }
+
+        return left;
+    }
+}
